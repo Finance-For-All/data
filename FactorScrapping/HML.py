43397bbf35e013a745d9ab6bb787daa7789dfb56
VFermat <vitor.eller30@gmail.com>
2020-03-29 14:23:41 -0300

    Added scrapper for HML, SMB, RF and Mkt-RF

---
+++ FactorScrapping/HML.py
@@ -0,0 +1,31 @@
+from zipfile import ZipFile
+from requests import get
+from io import BytesIO, StringIO
+from datetime import datetime
+import pandas as pd
+
+FREQUENCIES = ['daily', 'weekly', 'monthly']
+
+def HML(frequency='weekly', start_date='19260701', end_date=None):
+    
+    if frequency not in FREQUENCIES:
+        raise ValueError('Invalid value for frequency.')
+
+    if not end_date:
+        end_date = datetime.now().strftime('%Y%m%d')
+
+    try:
+        datetime.strptime(start_date, '%Y%m%d')
+    except:
+        raise ValueError('Invalid format for start_date')
+
+    url = f'http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_{frequency}_CSV.zip'
+    r = get(url, stream=True)
+    with ZipFile(BytesIO(r.content), 'r') as zip:
+        data = zip.read(zip.namelist()[0])
+
+    data = data.decode('utf-8')
+    df = pd.read_csv(pd.compat.StringIO(data), header=3, index_col=0)
+    df = df.query(f'index<\'{end_date}\' & index>\'{start_date}\'')
+    HML = df.HML
+    return HML
